91fd9f5bccd9f6053ef5c3f73e9a1ee93391ad21
Tasi Balázs <balazstasi2@gmail.com>
2023-06-30 03:18:40 +0200

    Removed unnecesary files

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,5 +1,1 @@
 export const RANDOM_WORD_API = "https://random-word-api.herokuapp.com/word?number=";
-export const SHRT_CODE_API = {
-  SHORTEN: "https://cors-anywhere.herokuapp.com/https://api.shrtco.de/v2/shorten?url=",
-  DETAILS: "https://cors-anywhere.herokuapp.com/https://api.shrtco.de/v2/info?code=",
-};
